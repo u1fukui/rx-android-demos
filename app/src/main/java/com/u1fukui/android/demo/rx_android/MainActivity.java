23ce32da0cb02ed696a7d3204aca07c84e9c2f40
u1 <u1.kobayashi@gmail.com>
2016-11-20 22:06:16 +0900

    最初のサンプルコードを追加

--- app/src/main/java/com/u1fukui/android/demo/rx_android/MainActivity.java
+++ app/src/main/java/com/u1fukui/android/demo/rx_android/MainActivity.java
@@ -2,12 +2,33 @@ package com.u1fukui.android.demo.rx_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        helloRx();
+    }
+
+    /**
+     * https://github.com/ReactiveX/RxJava/wiki/How-To-Use-RxJava
+     */
+    private void helloRx() {
+        String[] names = {"Ben", "George"};
+        Observable.from(names).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d(TAG, "Hello! " + s);
+            }
+        });
     }
 }
